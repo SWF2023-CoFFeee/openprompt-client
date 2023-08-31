232f5881fd43a6d427c82cd57406f8d95949d341
Jiheon Park <jhpark.a@imagoworks.ai>
2023-08-31 13:38:36 +0900

    chore: 컨트랙트파일 마이그레이션 자동화

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import { version } from 'hardhat';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    ganache: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 7722,
+    },
+  },
+};
+
+export default config;
