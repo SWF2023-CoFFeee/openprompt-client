f14a1e2cf18f5df6d7ae8e5250091f03d1facd46
Jiheon Park <jhpark.a@imagoworks.ai>
2023-08-31 13:50:29 +0900

    modify: change api url

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -4,9 +4,9 @@ export const LOGOUT_URL = '/api/v2/user/logout';
 export const TEST_URL = '/api/v2/user/test';
 
 export const DECODE_PROMPT_URL = '/api/v2/copyright';
-export const REGISTER_COPYRIGHT_URL = '/api/v2/copyright';
+export const REGISTER_COPYRIGHT_URL = '/api/v2/copyright/register';
 
 export const PRODUCT_LIST_URL = '/api/v2/product';
-export const REGISTER_PRODUCT_URL = '/api/v2/product/ticket';
+export const REGISTER_PRODUCT_URL = '/api/v2/product';
 export const BUY_COPYRIGHT_URL = '/api/v2/product/copyright';
-export const BUY_TICKET_URL = '/api/v2/product/copyright';
+export const BUY_TICKET_URL = '/api/v2/product/ticket';
